9b677e0353ac4e27a2e553f1111824f40d824daa
Lucas Vinicius Portela Leite <lucasvpldeveloper@gmail.com>
2024-06-04 22:03:21 -0300

    Ex 01 ao Ex 04 Python

---
+++ 03.py
@@ -0,0 +1,3 @@
+num1 = int(input('Digite um número:'))
+num2 =int(input('Digite outro número:'))
+print('A soma dos valores é', num1 + num2)
---
+++ 04.py
@@ -0,0 +1,8 @@
+nota1 = int(input('Qual foi a sua primeira nota do bimestre?'))
+nota2 = int(input('Qual foi a sua segunda nota do bimestre?'))
+nota3 = int(input('Qual foi a sua terceira nota do bimestre?'))
+nota4 = int(input('Qual foi a sua quarta nota do bimestre?'))
+nota = 4
+soma = nota1 + nota2 + nota3 + nota4 
+media = soma / nota 
+print('Média é', media)
