72efa9e98da95b890a4f88b0314787c35054da29
Lucas Vinicius Portela Leite <lucasvpldeveloper@gmail.com>
2024-06-04 22:46:17 -0300

    Exercício 05

---
+++ 05.py
@@ -0,0 +1,4 @@
+print ('Convertendo metros para centímetros')
+metro = float(input('Quantos metros você gostaria de converter para centímetros? '))
+cent = metro * 100
+print (metro,' metros equivale a:', cent, 'centímetros')
