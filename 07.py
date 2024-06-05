ee1361fca5ffcd87c2f7af72ba923d4a423f507e
Lucas Vinicius Portela Leite <lucasvpldeveloper@gmail.com>
2024-06-05 14:33:31 -0300

    Exercício 07

---
+++ 07.py
@@ -0,0 +1,5 @@
+print('Área do quadrado')
+area = float(input('Qual a medida da área do seu quadrado?'))
+tot1 = area ** 2
+tot2 = tot1 * 2 
+print('Aqui está! a área do seu quadro é =', tot1, 'o dobro da área do seu quadrado é igual a:', tot2)
