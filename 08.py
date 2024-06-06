78cbfe2e112e3d6606dd197728aebb8363b86119
Lucas Vinicius Portela Leite <lucasvpldeveloper@gmail.com>
2024-06-05 23:18:20 -0300

    Exercício 08

---
+++ 08.py
@@ -0,0 +1,4 @@
+valor = float(input("Quanto você ganha por hora?"))
+hora = float(input("Quantas horas você trabalhar por mês?"))
+total = valor * hora 
+print("Você ganha R$", valor, "por hora e trabalha", hora, 'horas por mês, logo seu salário é R$',total)
