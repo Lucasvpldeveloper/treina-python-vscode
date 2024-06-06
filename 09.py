b02c1681fdc7a83157accaf7d1687c008fce3650
Lucas Vinicius Portela Leite <lucasvpldeveloper@gmail.com>
2024-06-06 13:23:08 -0300

    Exercício 09 e 10

---
+++ 09.py
@@ -0,0 +1,4 @@
+print('Transformando Graus Fahrenheit em graus Celsius')
+fah = float(input('Quantos graus Fahrenheit você gostaria de converter? '))
+celsius = (fah - 32) * 5 / 9
+print('Temperatudo equivalente a', celsius, 'graus selsius')
---
+++ 10.py
@@ -0,0 +1,4 @@
+print('Transformando graus Celsius em Fahrenheit')
+celsius = float(input('Quantos graus celsius você gostaria de converter para fahrenheit? '))
+fah = (celsius * 9) / 5 + 32 
+print(celsius,'graus celsius é equivalente a', fah, 'fahrenheit')
