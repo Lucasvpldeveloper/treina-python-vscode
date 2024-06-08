1775c814fe91d56f831722a26871a0fbbee950de
Lucas Vinicius Portela Leite <lucasvpldeveloper@gmail.com>
2024-06-08 15:25:33 -0300

    Update 07sda.py

--- 07sda.py
+++ 07sda.py
@@ -1,7 +1,9 @@
 #!/usr / bin /python  
 # -* - coding : utf -8 -* -
+
 custo = float(input('Qual o valor do seu produto? '))
 porcentagem = float(input('Quantos % você gostaria de adicionar ao seu produto? '))
+
 tot1 = porcentagem / 100
 tot2 = tot1 * custo 
 tot3 = custo + tot2
